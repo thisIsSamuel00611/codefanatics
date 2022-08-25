95e717c2677164348deeaad4679b0fc0eb741f29
Samuel Emeka <samuelemeka2021@outlook.com>
2022-08-25 08:54:26 +0100

    add ang

--- codeFanaticsProject/src/app/app-routing.module.ts
+++ codeFanaticsProject/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './user/default/default.component';
+import { EditQuestionsComponent } from './admin/edit-questions/edit-questions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/', component: DefaultComponent },
+  { path: '/admin', component: EditQuestionsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
